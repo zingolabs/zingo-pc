d9e86291b10b76084b0a18248f67ccbb5a87c406
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2026-04-25 08:27:59 -0600

    fix: new release 2.0.14 (102)

--- bin/printversion.sh
+++ bin/printversion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-VERSION="2.0.14-101"
+VERSION="2.0.14-102"
 echo "VERSION=$VERSION" >> $GITHUB_ENV
--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-const APP_VERSION = "2.0.14 (101)";
+const APP_VERSION = "2.0.14 (102)";
 
 export default APP_VERSION;
