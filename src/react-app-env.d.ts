0ebf4b130ad71d4cf5d3742f57139b118cb079db
Juan Carlos Carmona Calvo <juanky201271@gmail.com>
2026-05-13 08:00:07 -0600

    fix: dead dependencies removed & report web vital make no sense here

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -40,15 +40,6 @@ declare module "*.webp" {
   export default src;
 }
 
-declare module "*.svg" {
-  import * as React from "react";
-
-  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
-
-  const src: string;
-  export default src;
-}
-
 declare module "*.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
