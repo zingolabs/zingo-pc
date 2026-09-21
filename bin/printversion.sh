4ce0368254150d305b5ac50e3a7f8d536ef7bc47
Juanky <juanky201271@gmail.com>
2026-09-21 11:03:51 -0600

    fix: new release 2.0.26 (189)

--- bin/printversion.sh
+++ bin/printversion.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-VERSION="2.0.26-188"
+VERSION="2.0.26-189"
 echo "VERSION=$VERSION" >> $GITHUB_ENV
--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-const APP_VERSION = "2.0.26 (188)";
+const APP_VERSION = "2.0.26 (189)";
 
 export default APP_VERSION;
